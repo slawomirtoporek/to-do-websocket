366c085e7a922ba00f3ed6a774200589909d4827
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-05-22 16:22:53 +0200

    add a listener on event connection

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const socket = reqiure('socket.io');
 
 const app = express();
 const server = app.listen(process.env.PORT || 8000, () => {
@@ -7,4 +8,26 @@ const server = app.listen(process.env.PORT || 8000, () => {
 
 app.get('*', (req, res) => {
   res.status(404).send({ message: 'Not found...' });
+});
+
+const io = socket(server);
+
+let tasks = [];
+
+io.on('connection', (socket) => {
+
+  socket.emit('updateData', tasks);
+
+  socket.on('addTask', (task) => {
+    tasks.push(task);
+    socket.broadcast.emit('addTask', task);
+  });
+
+  socket.on('removeTask', (idTask) => {
+    const index = tasks.findIndex(task => task.id === idTask);
+    if (index !== -1) {
+      socket.broadcast.emit('removeTask', idTask);
+      tasks.splice(index, 1);
+    };
+  });
 });
